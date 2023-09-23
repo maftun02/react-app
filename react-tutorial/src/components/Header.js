707d051afce5490c0d7dccad2d1eaed9644704be
maftun02 <Zikrillayevam97@gmail.com>
2023-09-23 18:25:55 +0500

    row

--- react-tutorial/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => {
-    return(
-        <header className="container-fluid"></header>
-    )
-}
-
-export default Header;
--- react-tutorial/src/components/NavBar.js
+++ react-tutorial/src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import img from './images/Vector.svg';
 const NavBar = () => {
         return(
+          <header className="container-fluid">
             <nav className="navbar navbar-expand-lg header_nav">
               <div className="container mt-3">
                 <a className="navbar-brand logo">MedicalFunc</a>
@@ -27,7 +28,24 @@ const NavBar = () => {
                 </div>
               </div>
             </nav>
-
+            <div className="container" style="margin-top: 100px;" >
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-5">
+                    <h5>Welcome</h5>
+                    <h1>Online Appoinment</h1>
+                    <h4>Medical Functional is most focused in helping you 
+                        discover your most beauiful smile</h4>
+                        <div className="cta">
+                            <a href="#" className="quote-now">Get Quote Now</a>
+                            <a href="#" className="learn">Learn More</a>
+                        </div>
+                </div>
+                <div className="col-md-7">
+                        <img src="react-tutorial/public/none.png"></img>
+                </div>
+            </div>
+        </div>
+         </header>
         )
     }
 
